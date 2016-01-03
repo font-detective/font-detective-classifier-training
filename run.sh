854c842e86f7ca7faf5921f1aee67c84c5a25619
lukem512 <lukem95@gmail.com>
2016-01-03 17:32:53 +0000

    Added training

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-opencv_createsamples
+./samples.sh
+opencv_haartraining -data haarcascade -vec samples.vec -bg negatives.lst -nstages 20 -nsplits 2 -minhitrate 0.999 -maxfalsealarm 0.5 -npos 7000 -nneg 3019 -w 20 -h 20 -nonsym -mem 512 -mode ALL
